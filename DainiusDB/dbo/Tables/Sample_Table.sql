fcb5aa01720bd6d5f0a781db9582eaa79273980a
dvaluzis <dvaluzis@opsecsecurityonline.com>
2023-01-20 10:18:19 +0200

    Ass SQL server files

---
+++ DainiusDB/dbo/Tables/Sample_Table.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Sample_Table] (
+    [id]       INT        NOT NULL,
+    [column_1] INT        NOT NULL,
+    [column_2] INT        NULL,
+    [column_3] INT        NULL,
+    [column_4] NCHAR (10) NULL,
+    CONSTRAINT [PK_Sample_Table] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
