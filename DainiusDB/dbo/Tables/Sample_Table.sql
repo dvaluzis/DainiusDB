503c736977952f176ce8851041f446ff8082b6b9
dvaluzis <dvaluzis@opsecsecurityonline.com>
2023-01-20 10:48:23 +0200

    Update table Sample_Table.sql

--- DainiusDB/dbo/Tables/Sample_Table.sql
+++ DainiusDB/dbo/Tables/Sample_Table.sql
@@ -2,8 +2,9 @@
     [id]       INT        NOT NULL,
     [column_1] INT        NOT NULL,
     [column_2] INT        NULL,
-    [column_3] INT        NULL,
-    [column_4] NCHAR (10) NULL,
+    [column_3] NCHAR (10) NULL,
     CONSTRAINT [PK_Sample_Table] PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
